836f5812e0b3250f8e6832da4c3eb056e9681232
tomderrym <tomderrymarionmantilla@gmail.com>
2026-01-06 09:27:24 +0800

    Add/update AppRouter component

---
+++ blocks/approuter-standalone.js
@@ -0,0 +1,46 @@
+/**
+ * AppRouter Component
+
+ */
+
+// Note: This component uses Tailwind CSS utility classes only.
+// No custom component library dependencies.
+// Ensure responsive (sm:, md:, lg:) and dark mode (dark:) classes are included.
+import React from 'https://esm.sh/react@18';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+
+
+
+
+
+
+export default function AppRouter: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/studio/:appId?"
+          element={
+            <ProtectedRoute>
+              <StudioPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
