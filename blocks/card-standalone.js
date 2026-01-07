399a926489bbbcbca180c215ef892e4a73ef6ea4
tomderrym <tomderrymarionmantilla@gmail.com>
2026-01-07 11:52:38 +0800

    Add/update Card component

--- blocks/card-standalone.js
+++ blocks/card-standalone.js
@@ -1,4 +1,5 @@
 import React from 'https://esm.sh/react@18';
+import { createElement } from 'https://esm.sh/react@18';
 
 interface CardProps {
   children: React.ReactNode;
@@ -13,15 +14,7 @@ interface CardProps {
  */
 const Card: React.FC<CardProps> = ({ children, className }) => {
   return (
-    <div
-      className={[
-        'bg-slate-900/70 border border-slate-800 rounded-2xl shadow-lg',
-        'px-4 py-4',
-        className ?? ''
-      ].join(' ')}
-    >
-      {children}
-    </div>
+    createElement('div', null, '{children}')
   );
 };
 
