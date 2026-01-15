b101edd78b33521838ff43de7cb67bc42883d533
tomderrym <tomderrymarionmantilla@gmail.com>
2026-01-16 03:50:45 +0800

    aa

--- Component90/Component90.js
+++
@@ -1,5 +0,0 @@
-import React, { createElement } from 'https://esm.sh/react@18';
-
-export default function Component90({  }) {
-  return createElement('div', { className: 'composite' }, children || []);
-}
