5b6ae43f54ad3ded98c95adbb83222f430bdc623
tomderrym <tomderrymarionmantilla@gmail.com>
2026-01-16 03:54:51 +0800

    aa

--- Composite/Composite.js
+++
@@ -1,5 +0,0 @@
-import React, { createElement } from 'https://esm.sh/react@18';
-
-export default function Composite({  }) {
-  return createElement('div', { className: 'composite' }, children || []);
-}
